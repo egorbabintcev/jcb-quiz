d60f5a609f186cdd8d1ad1cbe65fecbb47670769
Egor Babintsev <e.e.babintsev@yandex.ru>
2021-03-27 17:55:00 +0300

    Move all question data to another file. Add store object generation, based on json formatted questions

--- src/Stores/Store.js
+++ src/Stores/Store.js
@@ -7,104 +7,21 @@ import {
 import { v4 } from 'uuid';
 import { RadioQuestion, PictureRadioQuestion } from 'src/Stores/Question';
 
-const data = [
-  new PictureRadioQuestion('На какой технике работаете?', [
-    {
-      id: v4(),
-      title: '3CX, 4CX, 5CX',
-      isChecked: false,
-      imgSrc: 'cx-model.png',
-    },
-    {
-      id: v4(),
-      title: 'JC160-JC290',
-      isChecked: false,
-      imgSrc: 'jc-model.png',
-    },
-    {
-      id: v4(),
-      title: 'Другая модель JCB',
-      isChecked: false,
-      imgSrc: 'other-model.png',
-    },
-    {
-      id: v4(),
-      title: 'Нужна консультация',
-      isChecked: false,
-    },
-  ], '<strong>Запчасти от других моделей подходят только в 80% случаев.</strong><p>3CX – передние колеса маленькие</p><p>3CX super – одинаковые колеса, радиус 24 дюйма</p><p>4CX – одинаковые колеса, но радиус 26 дюймов</p><p>5CX – обновленная версия 4CX, выпускается с 2013 года</p><p>jc160-jc290 – частично совместимы с моделями выше</p>'),
-  new RadioQuestion('Какой год выпуска техники?', [
-    {
-      id: v4(),
-      title: 'До 2005 года',
-      isChecked: false,
-    },
-    {
-      id: v4(),
-      title: 'С 2005 года по настроящее время',
-      isChecked: false,
-    },
-    {
-      id: v4(),
-      title: 'Нужна консультация',
-      isChecked: false,
-    },
-  ], '<strong>В зависимости от года выпуска будут отличаться запчасти. Например было выпущено несколько двигателей.</strong><p>Если не знаете год выпуска техники, мы подкажем по WhatsApp как определиться.</p>'),
-  new PictureRadioQuestion('Где находится требуемая запчасть?', [
-    {
-      id: v4(),
-      title: 'Передняя или задняя стрела',
-      isChecked: false,
-      imgSrc: 'arm-part.png',
-    },
-    {
-      id: v4(),
-      title: 'Электрика',
-      isChecked: false,
-      imgSrc: 'wire-part.png',
-    },
-    {
-      id: v4(),
-      title: 'Кабина',
-      isChecked: false,
-      imgSrc: 'cabin-part.png',
-    },
-    {
-      id: v4(),
-      title: 'Двигатель',
-      isChecked: false,
-      imgSrc: 'engine-part.png',
-    },
-    {
-      id: v4(),
-      title: 'Гидравлика',
-      isChecked: false,
-      imgSrc: 'hydraulic-part.png',
-    },
-  ], '<strong>В зависимости от места нахождения необходимой запчасти</strong><p> можем предложить аналоговые запчасти собственного производства или оригинальные европейского качества</p>'),
-  new RadioQuestion('Как удобно получить запчасти?', [
-    {
-      id: v4(),
-      title: 'Доставка по Москве',
-      isChecked: false,
-    },
-    {
-      id: v4(),
-      title: 'Доставка по РФ в ваш город',
-      isChecked: false,
-    },
-    {
-      id: v4(),
-      title: 'Самовывоз из Одинцово',
-      isChecked: false,
-    },
-    {
-      id: v4(),
-      title: 'Другой',
-      isChecked: false,
-    },
-  ], '<ul><li><strong>По РФ или СНГ отправляем в день заказа любой транспорной компаний.</strong><br><small>До терминала привезем бесплатно</small></li><li><strong>По Москве доставим в день заказа курьером. Прямо к вам на объект или сервисный центр.</strong></li><li><strong>Все запчасти в наличии в магазине в Одинцово. Находится в 6 км от МКАД</strong></li><li><strong>Доставим любым удобным способом.</strong></li></ul>'),
-];
+const json = require('./data.json');
+
+const data = json.map((q) => {
+  const wrappedAnswers = q.answers.map((a) => ({ ...a, id: v4(), isChecked: false }));
+  switch (q.type) {
+    case 'default':
+      return new RadioQuestion(q.title, wrappedAnswers, q.hint);
+
+    case 'withPicture':
+      return new PictureRadioQuestion(q.title, wrappedAnswers, q.hint);
+
+    default:
+      return q;
+  }
+});
 
 class Store {
   @observable qtns = [...data];
